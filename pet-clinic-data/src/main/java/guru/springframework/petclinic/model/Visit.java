96116d0e95c1f67e221aadba0ecc7486b169b857
Piyush2509 <piyush.jayaswal@gmail.com>
2018-11-08 16:35:31 -0500

    Updated Visit with JPA. Closes #38.

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Visit.java
@@ -2,15 +2,27 @@ package guru.springframework.petclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 /**
  * Created by piyush.b.kumar on Nov 7, 2018
  */
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
-	private static final long serialVersionUID = 2517431389312768745L;
-
+	@Column(name = "date")
 	private LocalDate date;
+
+	@Column(name = "description")
 	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 
 	public LocalDate getDate() {
