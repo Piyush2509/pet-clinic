033e5b944dc846c3a50bf14c5ccc30df147a013a
Piyush2509 <piyush.jayaswal@gmail.com>
2018-11-08 14:20:06 -0500

    Implemented PetType service. Closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.model.PetType;
+
+/**
+ * Created by piyush.b.kumar on Nov 8, 2018
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
