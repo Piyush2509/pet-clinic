176662002e07e026b609ed854311f1a331ae30cd
Piyush2509 <piyush.jayaswal@gmail.com>
2018-11-09 12:18:46 -0500

    Created Visit map service. Closes #46.

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package guru.springframework.petclinic.services;
+
+import guru.springframework.petclinic.model.Visit;
+
+/**
+ * Created by piyush.b.kumar on Nov 9, 2018
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
