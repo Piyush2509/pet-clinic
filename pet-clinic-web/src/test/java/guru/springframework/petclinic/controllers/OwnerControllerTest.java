903ca0a5c146b523bca7a53a9768d01fcf0c80a1
Piyush2509 <piyush.jayaswal@gmail.com>
2018-11-19 13:03:14 -0500

    Fixed display owner issue. Closes #62

--- pet-clinic-web/src/test/java/guru/springframework/petclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/guru/springframework/petclinic/controllers/OwnerControllerTest.java
@@ -75,7 +75,7 @@ class OwnerControllerTest {
 	void testShowOwner() throws Exception {
 		when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
 
-		mockMvc.perform(get("/owners/123")).andExpect(status().isOk()).andExpect(view().name("owners/ownersDetails"))
+		mockMvc.perform(get("/owners/123")).andExpect(status().isOk()).andExpect(view().name("owners/ownerDetails"))
 				.andExpect(model().attribute("owner", hasProperty("id", is(1L))));
 	}
 
